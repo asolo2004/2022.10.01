aff2c981b94c55bfb2b0a19f66daf773426310b5
Solovev, Andrew <qwertauyuuiu@gmail.com>
2022-10-07 18:42:40 +0300

    Homework

---
+++ 1.py
@@ -0,0 +1,10 @@
+# Написал оба варианта кода: с вводом, с зараннее заданными переменными
+# name = input("Введите имя:")
+# surname = input("Введите фамилию:")
+# year_of_birth=int(input("Введите год рождения:"))
+name = 'Андрей'
+surname = 'Соловьев'
+year_of_birth = 2004
+print(name+'_'+surname+'_'+str(year_of_birth))
+year_of_birth += 60
+print(surname+'_'+name+'_'+str(year_of_birth))
